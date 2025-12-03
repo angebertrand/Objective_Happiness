f43cb8896277da06ee47ab0fc61d52db240820f3
Kerian <kerian.gouriou@gmail.com>
2025-12-03 13:50:50 +0100

    Tile Position System Working

--- Assets/Scripts/TileSettings.cs
+++ Assets/Scripts/TileSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -28,31 +29,29 @@ public class TileSettings : MonoBehaviour
     {
         MeshRenderer mr = GetComponentInChildren<MeshRenderer>();
 
-        if (mr == null)
-        {
-            Debug.LogError("Aucun MeshRenderer trouvé sur l'hexagone !");
-            return 1f;
-        }
-
         // taille réelle du mesh rendu
         Vector3 size = mr.bounds.size;
 
-        // Pour un hex pointy-top :
-        // largeur = radius * √3
-        // radius = largeur / √3
+        // Pour un hex flat-top :
+        // largeur = radius * 2
+        // radius = largeur / 2
         float width = size.x;
 
-        float radius = width / Mathf.Sqrt(3f);
+        float radius = width / 2;
 
         return radius;
     }
 
     Vector3 PlacerHexaDansWorld(int x, int y, float size)
     {
-        float worldX = size * (1.5f * x); 
-        float worldZ = size * (Mathf.Sqrt(3f) * (y + x * 0.5f)); 
+        float worldX = size * 3 * x;
+        float worldZ = (size * (float)Math.Sqrt(3f))/2 * y;
+        if (y % 2 == 1)
+        {
+            worldX += size * 1.5f;
+        }
 
-        return new Vector3(worldX, 0f, worldZ); 
+        return new Vector3(worldX, 0f, -worldZ); 
     }
 
     public void ressourceIsExtracted()
